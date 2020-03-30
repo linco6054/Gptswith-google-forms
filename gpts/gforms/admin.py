a2730bb67c39ba77bdf6550fc5c0f3691b17f086
unknown <lincoln.kantet@gmail.com>
2020-03-30 14:45:19 +0300

    updated

--- gpts/gforms/admin.py
+++ gpts/gforms/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from . models import User
+from . models import User, school , profile 
+from front.models import champions
 # Register your models here.
 admin.site.register(User)
+admin.site.register(school)
+admin.site.register(profile)
+admin.site.register(champions)
